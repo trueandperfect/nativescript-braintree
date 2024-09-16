dd5d34c586b41fe972d8d50b2a82ed929a092770
John Bradshaw <trueandperfect@gmail.com>
2024-09-16 12:30:07 -0400

    moved src folder contents to root

--- demo/app/app.ts
+++
@@ -1,8 +0,0 @@
-﻿import { setupBraintreeAppDeligate } from "nativescript-braintree";
-import {Application, isIOS} from "@nativescript/core";
-
-if (isIOS) {
-    setupBraintreeAppDeligate("org.nativescript.demo.payments");
-}
-
-Application.run({ moduleName: "main-page" });
--- demo/app/main-page.ts
+++
@@ -1,8 +0,0 @@
-import {EventData, Page} from "@nativescript/core";
-import { HelloWorldModel } from './main-view-model';
-
-export function pageLoaded(args: EventData) {
-    // Get the event sender
-    let page = <Page>args.object;
-    page.bindingContext = new HelloWorldModel();
-}
